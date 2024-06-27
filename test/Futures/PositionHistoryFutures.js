e0182fb25223aa8ea6f3c7d14b551f7cc9c5d231
shivamcp10 <shivamcp10@gmail.com>
2024-06-27 17:01:04 +0530

    1. Futures Assets and Position History added and tested : done

---
+++ test/Futures/PositionHistoryFutures.js
@@ -0,0 +1,25 @@
+const Future = require('../../src/future');
+require('dotenv').config();
+
+const apiKey = process.env.MEXC_API_KEY_15_01_24;
+const apiSecret = process.env.MEXC_SECRETE_15_01_24;
+
+const client = new Future(apiKey, apiSecret);
+
+// Function to get assets
+function getPositionHistory(options = {}) {
+    return new Promise((resolve, reject) => {
+      client.HistoryPositions(options)
+        .then(response => {
+          resolve({ exchange: 'mexc', positionInfo: response.data });
+        })
+        .catch(error => {
+          reject({ error: 'Error fetching position history information' });
+        });
+    });
+  }
+  
+  module.exports = {
+    getPositionHistory,
+  };
+  
